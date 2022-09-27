979593d21728619881ac9e2b9692dfe6623eb176
Parth Parmar <pp073522@gmail.com>
2022-09-27 21:14:15 +0530

    minor fix

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use(express.json());
 const authorization = require("./middleware/auth");
 
 //for production
+const path = require('path')
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
@@ -55,7 +56,6 @@ var server = app.listen(port, () => {
   console.log("app started");
 });
 
-
 //socket io configuration
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
@@ -66,46 +66,45 @@ const io = require("socket.io")(server, {
 
 //event handling
 io.on("connection", (socket) => {
-
   socket.on("joinChat", (id) => {
     socket.join(id);
     socket.emit("chatJoined", id);
   });
 
-  socket.on('joinRoom' , (id) => {
-    socket.join(id)
-    socket.emit('roomJoined' , id)
-  })
+  socket.on("joinRoom", (id) => {
+    socket.join(id);
+    socket.emit("roomJoined", id);
+  });
 
-  socket.on('sendMessage' , (msg) =>{
-    msg.convo_id.members.forEach(m => {
-      if(m == msg.sender._id) return
-      socket.in(m).emit('receiveMessage' , msg)
-    })
-  })
+  socket.on("sendMessage", (msg) => {
+    msg.convo_id.members.forEach((m) => {
+      if (m == msg.sender._id) return;
+      socket.in(m).emit("receiveMessage", msg);
+    });
+  });
 
-  socket.on('imTyping' , (obj) => {
-    socket.to(obj.id).emit('isTyping' , (obj))
-  })
-  socket.on('imNotTyping' , (id) => {
-    socket.to(id).emit('isNotTyping' , id)
-  })
+  socket.on("imTyping", (obj) => {
+    socket.to(obj.id).emit("isTyping", obj);
+  });
+  socket.on("imNotTyping", (id) => {
+    socket.to(id).emit("isNotTyping", id);
+  });
 
-  socket.on('clearingChat' , (obj) => {
-    socket.to(obj.id).emit('chatCleared' , obj)
-  })
-  socket.on('deleteChat' , (obj) => {
-    socket.to(obj.id).emit('chatDeleted' , obj)
-  })
+  socket.on("clearingChat", (obj) => {
+    socket.to(obj.id).emit("chatCleared", obj);
+  });
+  socket.on("deleteChat", (obj) => {
+    socket.to(obj.id).emit("chatDeleted", obj);
+  });
 
-  socket.on('addMember' , (obj) => {
-    socket.to(obj._id).emit("memberAdded" , obj)
-  })
+  socket.on("addMember", (obj) => {
+    socket.to(obj._id).emit("memberAdded", obj);
+  });
 
-  socket.on('removeMember' , (obj)=>{
-    const id = obj.convo._id
-    socket.to(id).emit("memberRemoved" , obj)
-
-  })
+  socket.on("removeMember", (obj) => {
+    const id = obj.convo._id;
+    socket.to(id).emit("memberRemoved", obj);
+  });
 });
+
 
